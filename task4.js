e1011b93fd827a0bf8a27fd86574034c9315a147
Vivekkumar0412 <www.vivekkumar0412@gmail.com>
2023-04-15 22:42:26 +0530

    question on map method

--- task4.js
+++ task4.js
@@ -88,78 +88,88 @@
 // console.log(products_value);
 // for(let i = 0; i<products_value.length; i++){
 //     let h1 = document.createElement("p");
-//     h1.setAttribute("class","h11");
-//     h1.append(`Product is ${products_value[i].name} and it's value is ${products_value[i].value}`);
-//     document.body.appendChild(h1);
-// }
-// // // console.log(products);
+// //     h1.setAttribute("class","h11");
+// //     h1.append(`Product is ${products_value[i].name} and it's value is ${products_value[i].value}`);
+// //     document.body.appendChild(h1);
+// // }
+// // // // console.log(products);
+
+// let arr = [10, 2, 3, 40, 5, 6];
+// let arr2 = arr.reduce((a, b) => {
+//   return a + b;
+// });
+
+// let ar3 = arr.reduce((acc, crr) => {
+//   // debugger;
+//   if (crr > acc) {
+//     return crr;
+//   } else {
+//     return acc;
+//   }
+// });
+// console.log(ar3);
+
+// arr.sort((a, b) => {
+//   return a - b;
+// });
+
+// console.log(arr[arr.length - 1]);
+
+// let sr = ["vivek", "om", "pranav"];
+// let sr2 = sr.reduce((a, c) => {
+//   if (a.length < c.length) {
+//     return a;
+//   } else {
+//     return c;
+//   }
+// });
+
+// console.log(sr2);
+
+// let products = [
+//   { name: "dove", price: 100, quant: 10 },
+//   { name: "lux", price: 10, quant: 10 },
+//   { name: "medimix", price: 10, quant: 10 },
+//   { name: "pears", price: 800, quant: 10 },
+// ];
+
+// let valu = products.reduce((a, c) => {
+//   let curr_val = c.price * c.quant;
+//   let acc_val = a.price * a.quant;
+//   if (acc_val < curr_val) {
+//     return c;
+//   } else {
+//     return a;
+//   }
+// });
+
+// console.log(valu);
+
+// let data = [
+//   { name: "Jack", country: "USA", age: 35 },
+//   { name: "Amit", country: "India", age: 38 },
+//   { name: "Edward", country: "USA", age: 41 },
+//   { name: "Vishal", country: "India", age: 30 },
+//   { name: "Annie", country: "USA", age: 27 },
+//   { name: "Nick", country: "France", age: 32 },
+//   { name: "Francis", country: "France", age: 44 },
+//   { name: "Preeti", country: "India", age: 25 },
+//   { name: "Sophie", country: "France", age: 29 },
+//   { name: "Harpreet", country: "India", age: 48 },
+//   { name: "Bob", country: "USA", age: 21 },
+// ];
+// console.log(data.filter((elem)=>{
+
+//     return elem.country == "USA" && elem.age > 27;
+// }));
+
+// Task 2 Given an array of numbers,
+// create a new array whose numbers are twice the number of the input array. Use map
 
 let arr = [10, 2, 3, 40, 5, 6];
-let arr2 = arr.reduce((a, b) => {
-  return a + b;
-});
+console.log(
+  arr.map((elem) => {
+    return elem * 2;
+  })
+);
 
-let ar3 = arr.reduce((acc, crr) => {
-  // debugger;
-  if (crr > acc) {
-    return crr;
-  } else {
-    return acc;
-  }
-});
-console.log(ar3);
-
-arr.sort((a, b) => {
-  return a - b;
-});
-
-console.log(arr[arr.length - 1]);
-
-let sr = ["vivek", "om", "pranav"];
-let sr2 = sr.reduce((a, c) => {
-  if (a.length < c.length) {
-    return a;
-  } else {
-    return c;
-  }
-});
-
-console.log(sr2);
-
-let products = [
-  { name: "dove", price: 100, quant: 10 },
-  { name: "lux", price: 10, quant: 10 },
-  { name: "medimix", price: 10, quant: 10 },
-  { name: "pears", price: 800, quant: 10 },
-];
-
-let valu = products.reduce((a, c) => {
-  let curr_val = c.price * c.quant;
-  let acc_val = a.price * a.quant;
-  if (acc_val < curr_val) {
-    return c;
-  } else {
-    return a;
-  }
-});
-
-console.log(valu);
-
-let data = [
-  { name: "Jack", country: "USA", age: 35 },
-  { name: "Amit", country: "India", age: 38 },
-  { name: "Edward", country: "USA", age: 41 },
-  { name: "Vishal", country: "India", age: 30 },
-  { name: "Annie", country: "USA", age: 27 },
-  { name: "Nick", country: "France", age: 32 },
-  { name: "Francis", country: "France", age: 44 },
-  { name: "Preeti", country: "India", age: 25 },
-  { name: "Sophie", country: "France", age: 29 },
-  { name: "Harpreet", country: "India", age: 48 },
-  { name: "Bob", country: "USA", age: 21 },
-];
-console.log(data.filter((elem)=>{
-    
-
-    return elem.country == "USA" && elem.age > 27;
-}));
